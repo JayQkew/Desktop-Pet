fe6bf1ff1d362a7c0a619c684b4241674d9b4640
JayQkew <rayjebb@gmail.com>
2025-04-16 20:42:22 +0200

    Adding transparent window

---
+++ Desktop Pet/Assets/_Scripts/TransparentWindow.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TransparentWindow : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+    
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+    
+    [DllImport("user32.dll")]
+    static extern int SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+    
+    const int GWL_EXSTYLE = -20;
+    const int WS_EX_LAYERED = 0x00080000;
+    const int WS_EX_TRANSPARENT = 0x00000020;
+    
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    
+    const uint LWA_COLORKEY = 0x00000001;
+
+    private IntPtr hWnd;
+    private void Start() {
+#if !UNITY_EDITOR
+        hWnd = GetActiveWindow();
+
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 };
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+        
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        // SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+        
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, WS_EX_TRANSPARENT);
+#endif
+        Application.runInBackground = true;
+    }
+
+    private void Update() {
+        SetClickthrough(Physics2D.OverlapPoint(Mouse.current.position.ReadValue()) == null);
+    }
+
+    private void SetClickthrough(bool clickthrough) {
+        if (clickthrough) {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        else {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        }
+    }
+}
