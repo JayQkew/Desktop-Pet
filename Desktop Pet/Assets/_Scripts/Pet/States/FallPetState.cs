5565b19ae257883480d1e919ab78e49a9708a69d
JayQkew <rayjebb@gmail.com>
2025-04-29 12:19:36 +0200

    frog fall slowwer

--- Desktop Pet/Assets/_Scripts/Pet/States/FallPetState.cs
+++ Desktop Pet/Assets/_Scripts/Pet/States/FallPetState.cs
@@ -6,17 +6,28 @@ public class FallPetState : PetBaseState
 {
     [SerializeField] private Color color = Color.white;
     [SerializeField] private LayerMask floorMask;
+    [SerializeField] private float decelerationSpeed = 1f;
+    [SerializeField] private float fallSpeed = 1f;
     private Transform _transform;
+    private Rigidbody2D rb;
 
     public override void EnterState(PetStateManager manager) {
+        if(!rb) rb = manager.GetComponent<Rigidbody2D>();
         manager.GetComponentInChildren<SpriteRenderer>().color = color;
         _transform = manager.transform;
     }
 
     public override void UpdateState(PetStateManager manager) {
         //do a ground check and velocity check
-        if (Grounded() && manager.GetComponent<Rigidbody2D>().linearVelocity == Vector2.zero) {
+        if (Grounded() && rb.linearVelocity == Vector2.zero) {
             manager.SwitchState(PetState.Idle);
+        }
+
+        if (rb.linearVelocityY < 0) {
+            //clamp the y velocity
+            //lerp the x velocity to 0
+            rb.linearVelocityY = Mathf.Lerp(rb.linearVelocityY, fallSpeed, decelerationSpeed * Time.deltaTime);
+            rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, 0, Time.deltaTime * decelerationSpeed);
         }
     }
 
