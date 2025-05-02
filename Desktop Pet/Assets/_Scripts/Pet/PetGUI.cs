0d98f9a054bd8deb8aa72f44d03ca212819a840d
JayQkew <rayjebb@gmail.com>
2025-05-02 19:50:06 +0200

    remove debug

--- Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
+++ Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
@@ -9,8 +9,5 @@ public class PetGUI : MonoBehaviour
         _anim = GetComponent<Animator>();
     }
 
-    public void SetAnim(PetState state) {
-        Debug.Log("set anim to " + state);
-        _anim.SetInteger("State", (int)state);
-    }
+    public void SetAnim(PetState state) => _anim.SetInteger("State", (int)state);
 }
