892388d2c89a3c3914592fb9cdccccae0c691fb0
Michael-Naidoo <2583482@students.wits.ac.za>
2025-05-04 23:03:11 +0200

    Michael's test | alterations
    increased number of held sprites,
    Added a network identity to the timer arm,
    Implemented an adidtional farm state

--- Desktop Pet/Assets/_Scripts/PlantData.cs
+++ Desktop Pet/Assets/_Scripts/PlantData.cs
@@ -5,5 +5,9 @@ public class PlantData : ScriptableObject
 {
     public string plantName;
     public float growthTimeSeconds;
+    public Sprite planted;
+    public Sprite growing;
+    public Sprite finished;
+    public Sprite rotten;
     // Add other plant-specific data like money earned.
 }
