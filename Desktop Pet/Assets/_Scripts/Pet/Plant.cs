545953906e2283016e3b0ad6fafcb716322dded2
JayQkew <rayjebb@gmail.com>
2025-05-04 22:44:42 +0200

    Adding plants shootables

---
+++ Desktop Pet/Assets/_Scripts/Pet/Plant.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Plant : MonoBehaviour, IInteractable
+{
+    public GameObject petTarget;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask petLayer;
+    [SerializeField] private Vector2 velClamp;
+
+    private Rigidbody2D rb;
+    private Vector2 prevPos;
+    private Vector2 currVel;
+
+    private float _velocityTick = 0.01f;
+    private float _currTime = 0;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update() {
+        SeedRadius();
+    }
+
+    private void SeedRadius() {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, radius, Vector2.zero, 0, petLayer);
+        if (hit && !petTarget) {
+            petTarget = hit.collider.gameObject;
+            petTarget.GetComponent<Pet>().Food(gameObject);
+        }
+    }
+
+    public void OnLeftPickup() {
+        rb.linearVelocity = Vector2.zero;
+        prevPos = transform.position;
+        currVel = Vector2.zero;
+    }
+
+    public void OnLeftDrop() {
+        float clampedX = Mathf.Clamp(currVel.x, -velClamp.x, velClamp.x);
+        float clampedY = Mathf.Clamp(currVel.y, -velClamp.y, velClamp.y);
+        Vector2 clampedVel = new Vector2(clampedX, clampedY);
+        rb.linearVelocity = clampedVel;
+    }
+
+    public void OnLeftHeld(Vector2 offset) {
+        rb.MovePosition(offset);
+        rb.linearVelocity = Vector2.zero;
+        _currTime += Time.deltaTime;
+        if (_currTime >= _velocityTick) {
+            Vector2 currPos = transform.position;
+            currVel = (currPos - prevPos)/_currTime;
+            prevPos = currPos;
+            _currTime = 0;
+        }
+    }
+
+    public void OnRightPickup() {
+    }
+
+    public void OnRightDrop() {
+    }
+
+    public void OnRightHeld(Vector2 offset) {
+    }
+
+}
