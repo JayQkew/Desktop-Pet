6a9de68f315448421730bda02ae5724c1a66339e
JayQkew <rayjebb@gmail.com>
2025-05-05 16:59:57 +0200

    animator for hat

--- Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
+++ Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class PetGUI : MonoBehaviour
 {
     private Animator _anim;
+    private Animator _hatAnim;
     private PetStateManager _petStateManager;
     [SerializeField] private SpriteRenderer _hatSpriteRenderer;
     
 
     private void Awake() {
         _anim = GetComponent<Animator>();
+        _hatAnim = GetComponentInChildren<Animator>();
     }
 
-    public void SetAnim(PetState state) => _anim.SetInteger("State", (int)state);
+    public void SetAnim(PetState state) {
+        _anim.SetInteger("State", (int)state);
+        _hatAnim.SetInteger("State", (int)state);
+    }
     
     public void SetHatSprite(Sprite sprite) => _hatSpriteRenderer.sprite = sprite;
 }
