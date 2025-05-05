d88d9443a16f8eb03596a1889752b6a75a1cad38
JayQkew <rayjebb@gmail.com>
2025-05-05 19:00:07 +0200

    better GUI flipping and hat animations

--- Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
+++ Desktop Pet/Assets/_Scripts/Pet/PetGUI.cs
@@ -1,22 +1,22 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PetGUI : MonoBehaviour
 {
     private Animator _anim;
-    private Animator _hatAnim;
+    public Animator hatAnim;
     private PetStateManager _petStateManager;
     [SerializeField] private SpriteRenderer _hatSpriteRenderer;
     
 
     private void Awake() {
         _anim = GetComponent<Animator>();
-        _hatAnim = GetComponentInChildren<Animator>();
     }
 
     public void SetAnim(PetState state) {
         _anim.SetInteger("State", (int)state);
-        _hatAnim.SetInteger("State", (int)state);
+        hatAnim.SetInteger("State", (int)state);
     }
     
     public void SetHatSprite(Sprite sprite) => _hatSpriteRenderer.sprite = sprite;
